f0bc6d99933bf484b4095d29bb32b88919d949c0
Tarun Thakur <tarunt5555@gmail.com>
2026-02-09 21:15:43 +0530

    Add cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const logsRoute =  require('./routes/logs.routes');
 const express = require('express');
 const { verifyToken } = require('./middleware/verifyToken');
 const app = express();
+const cors = require('cors');
+app.use=(cors())
 app.use(express.json()); 
 
 app.use('/auth',authRoute);
