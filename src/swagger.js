0924e7e373e1c82b8528d9829d98772508b0dda7
Tarun Thakur <tarunt5555@gmail.com>
2026-02-10 22:15:09 +0530

    inastall Swagger to build Api documentation

--- src/swagger.js
+++ src/swagger.js
@@ -5,9 +5,19 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Backend API",
+      title: "API Monitoring Tool 🛠️",
       version: "1.0.0",
-      description: "Auto-generated API documentation",
+      description: `Our API Monitoring Tool is designed to help backend developers keep a close eye on their APIs’ health and performance. With this tool, you can:
+
+Track API uptime and crashes – know exactly when and how often your APIs fail.
+
+Monitor response times – see the average response time for each API call.
+
+Identify slow endpoints – quickly find which endpoints are causing delays.
+
+Gain actionable insights – make informed decisions to optimize your API performance.
+
+Keep your APIs reliable, efficient, and user-friendly with real-time monitoring and detailed analytics.`,
     },
     servers: [
       {
